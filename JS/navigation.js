66cb401c42fcec021f1cee86f4a64e264f244ba7
Akpan joseph <syntaxdesigners@gmail.com>
2023-11-30 16:40:09 +0100

    done with navigation

--- JS/navigation.js
+++ JS/navigation.js
@@ -94,12 +94,13 @@ const appHeader = `
 
 
 // app footer
-const appFooter =`
+const appFooter = `
 <div
-class="shadow-sm relative bottom-0 pt-20 pb-28 px-4 w-full bg-white lg:px-24"
+class="shadow-sm relative bottom-0 pt-20 pb-28 px-4 w-full bg-white lg:pb-20 lg:px-20"
 >
 <!-- first footer section container -->
 <div class="grid grid-cols-1 w-full gap-10 lg:flex lg:justify-evenly">
+
   <!-- footer logo -->
   <div class="w-[70px] h-[20px]">
     <svg width="99" height="22" viewBox="0 0 99 22" fill="none">
@@ -183,7 +184,7 @@ class="shadow-sm relative bottom-0 pt-20 pb-28 px-4 w-full bg-white lg:px-24"
 
 <!-- first footer section container end -->
 
-<div class="flex gap-2 my-6 lg:gap-4 lg:my-4">
+<div class="flex  gap-2 my-6 lg:gap-4 lg:my-4 lg:px-20">
   <!-- google card -->
   <div>
     <svg width="135" height="40" viewBox="0 0 135 40" fill="none">
@@ -643,7 +644,7 @@ class="shadow-sm relative bottom-0 pt-20 pb-28 px-4 w-full bg-white lg:px-24"
 </p>
 
 <!-- contact info section -->
-<div class="grid grid-cols-1 gap-4 mt-10 lg:gap-0 lg:flex lg:justify-between ">
+<div class="grid grid-cols-1 gap-4 mt-10 lg:gap-0 lg:flex lg:justify-between lg:px-20">
   <div class="text-primary gap-4 flex flex-col">
     <h1 class="font-bold">Contact</h1>
     <a href="" class="underline">help@kuda.com </a>
@@ -652,25 +653,25 @@ class="shadow-sm relative bottom-0 pt-20 pb-28 px-4 w-full bg-white lg:px-24"
       <div
         class="border border-primary w-8 h-8 flex items-center justify-center bg-[#EFF1FF] rounded-full"
       >
-        <img src="./asserts/icon/x-twitter.svg" alt="" />
+      <svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" fill="#40196D" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.--><path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"/></svg>
       </div>
 
       <div
         class="border border-primary w-8 h-8 flex items-center justify-center bg-[#EFF1FF] rounded-full"
       >
-        <img src="./asserts/icon/instagram.svg" alt="" />
+      <svg xmlns="http://www.w3.org/2000/svg" fill="#40196D" height="16" width="14" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.--><path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"/></svg>
       </div>
 
       <div
         class="border border-primary w-8 h-8 flex items-center justify-center bg-[#EFF1FF] rounded-full"
       >
-        <img src="./asserts/icon/facebook-f.svg" alt="" />
+      <svg xmlns="http://www.w3.org/2000/svg" fill="#40196D" height="16" width="10" viewBox="0 0 320 512"><!--!Font Awesome Free 6.5.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.--><path d="M80 299.3V512H196V299.3h86.5l18-97.8H196V166.9c0-51.7 20.3-71.5 72.7-71.5c16.3 0 29.4 .4 37 1.2V7.9C291.4 4 256.4 0 236.2 0C129.3 0 80 50.5 80 159.4v42.1H14v97.8H80z"/></svg>
       </div>
 
       <div
         class="border border-primary w-8 h-8 flex items-center justify-center bg-[#EFF1FF] rounded-full"
       >
-        <img src="./asserts/icon/youtube.svg" alt="" />
+      <svg xmlns="http://www.w3.org/2000/svg" fill="#40196D" height="16" width="18" viewBox="0 0 576 512"><!--!Font Awesome Free 6.5.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.--><path d="M549.7 124.1c-6.3-23.7-24.8-42.3-48.3-48.6C458.8 64 288 64 288 64S117.2 64 74.6 75.5c-23.5 6.3-42 24.9-48.3 48.6-11.4 42.9-11.4 132.3-11.4 132.3s0 89.4 11.4 132.3c6.3 23.7 24.8 41.5 48.3 47.8C117.2 448 288 448 288 448s170.8 0 213.4-11.5c23.5-6.3 42-24.2 48.3-47.8 11.4-42.9 11.4-132.3 11.4-132.3s0-89.4-11.4-132.3zm-317.5 213.5V175.2l142.7 81.2-142.7 81.2z"/></svg>
       </div>
 
       <div
@@ -682,7 +683,7 @@ class="shadow-sm relative bottom-0 pt-20 pb-28 px-4 w-full bg-white lg:px-24"
       <div
         class="border border-primary w-8 h-8 flex items-center justify-center bg-[#EFF1FF] rounded-full"
       >
-        <img src="./asserts/icon/linkedin-in.svg" alt="" />
+      <svg xmlns="http://www.w3.org/2000/svg" fill="#40196D" height="16" width="14" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.--><path d="M100.3 448H7.4V148.9h92.9zM53.8 108.1C24.1 108.1 0 83.5 0 53.8a53.8 53.8 0 0 1 107.6 0c0 29.7-24.1 54.3-53.8 54.3zM447.9 448h-92.7V302.4c0-34.7-.7-79.2-48.3-79.2-48.3 0-55.7 37.7-55.7 76.7V448h-92.8V148.9h89.1v40.8h1.3c12.4-23.5 42.7-48.3 87.9-48.3 94 0 111.3 61.9 111.3 142.3V448z"/></svg>
       </div>
     </div>
   </div>
@@ -710,7 +711,7 @@ class="shadow-sm relative bottom-0 pt-20 pb-28 px-4 w-full bg-white lg:px-24"
 <!-- contact info section end-->
 
 <!-- terms of use section -->
-<div class="flex flex-col gap-4 mt-16 text-[#979797] text-sm">
+<div class="flex flex-col gap-4 mt-16 text-[#979797] text-sm lg:px-20">
   <div>
     <p>*Kuda Customers get 25 free transfer to other banks every month. Extra transfers to other banks cost ₦10 each.</p>
     <p>**Kuda account holders can deposit money in over 10,000 branches across Nigeria for free.</p>
@@ -728,6 +729,7 @@ class="shadow-sm relative bottom-0 pt-20 pb-28 px-4 w-full bg-white lg:px-24"
 </div>
 <!-- terms of use section end-->
 </div>
+
 <!-- ad section -->
 <div class="bg-[#EFF1FF] fixed w-full bottom-0 flex justify-between py-4 px-4 items-center lg:hidden ">
 <div class="w-[70px] h-[20px]">
@@ -760,9 +762,9 @@ let footer = document.querySelector("footer");
 let header = document.querySelector("header");
 
 
-if(header){
-    header.innerHTML = appHeader
-}else alert('Could not find header')
+if (header) {
+  header.innerHTML = appHeader
+} else alert('Could not find header')
 
 if (footer) {
   footer.innerHTML = appFooter;
